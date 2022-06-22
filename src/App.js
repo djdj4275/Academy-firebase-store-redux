a7d4b6e803024dccf92a322c886f6461dfa108e9
junyong <djdj4275@naver.com>
2022-06-22 12:29:48 +0900

    'firebasestore'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Data from "./components/data";
+import UserData from "./components/UserData";
+
+function App() {
+  return (
+    <div className="App">
+      <UserData />
+      <Data />
+    </div>
+  );
+}
+
+export default App;
